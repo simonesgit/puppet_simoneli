4ed899a37b74ea974ca08cff1d46f7a2cd37b577
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-03 14:03:06 +0800

    Create servcie.py

---
+++ servcie.py
@@ -0,0 +1,40 @@
+# Shared Agent Cluster IT Service Offering
+
+## Basic Information
+
+| Feature                   | Details                                         |
+|---------------------------|-------------------------------------------------|
+| **Availability Status**   | Pilot                                           |
+| **Required Software Component** | -                                       |
+| **Supported Application Kind** | Serverless                                  |
+| **Agent Versions**        | [List available agent versions]                 |
+| **Job Types**             | [List supported job types]                      |
+
+## Introduction
+
+Our Shared Agent Cluster IT Service is an innovative solution that allows application teams to schedule serverless jobs without the need to install and manage agent software on their servers. This service aims to improve efficiency and reduce the workload for application teams. With a centralized, shared agent cluster, you can streamline the execution of serverless tasks and focus on developing high-quality applications.
+
+## Target Use Cases
+
+The Shared Agent Cluster IT Service is ideal for the following use cases:
+
+1. **Batch Executable By API**: Schedule and manage batch jobs efficiently through API calls, reducing manual intervention and improving overall application performance.
+2. **Semaphores Fetch by API**: Seamlessly fetch semaphores using API calls, simplifying the coordination of parallel tasks and ensuring smooth task execution.
+3. **Control-M Plugins with Centralized Connection Profiles**: Leverage Control-M plugins supported by centralized connection profiles, improving the management of connections for various applications and services.
+
+## Service Limitations
+
+Please be aware of the following limitations of the Shared Agent Cluster IT Service:
+
+1. **Job Output Retention**: As the service runs on the Internal Kubernetes (k8s) platform, we cannot guarantee that user job output will always be retained for a specific number of days. Users are encouraged to ensure they have appropriate backup and retention policies in place for their job outputs.
+
+## Onboarding Procedure
+
+To get started with the Shared Agent Cluster IT Service, follow the onboarding procedure outlined below:
+
+1. **Jira Service Desk**: Access our Jira Service Desk and create a new request to onboard the Shared Agent Cluster IT Service.
+2. **Application Details**: Provide the required information about your application, including the supported application kind, agent version, and job types.
+3. **Approval**: Our team will review your request and approve it if it meets the criteria for the Shared Agent Cluster IT Service.
+4. **Access**: Once approved, you will be granted access to the Shared Agent Cluster IT Service, allowing you to schedule and manage serverless jobs for your application.
+
+To begin the onboarding process, please visit our [Jira Service Desk](https://yourcompany.atlassian.net/servicedesk/).

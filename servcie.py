4e64540cbffb97a241806a25fb9f731cb16197a5
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-10 11:44:18 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -1,40 +1,37 @@
-# Shared Agent Cluster IT Service Offering
+1.Introduction
 
-## Basic Information
+Overview of the Control-M/Agent clusters and their purpose
+Description of the Internal K8s platform (IKP) and its role in hosting the Control-M/Agent clusters
+Architecture and Components
 
-| Feature                   | Details                                         |
-|---------------------------|-------------------------------------------------|
-| **Availability Status**   | Pilot                                           |
-| **Required Software Component** | -                                       |
-| **Supported Application Kind** | Serverless                                  |
-| **Agent Versions**        | [List available agent versions]                 |
-| **Job Types**             | [List supported job types]                      |
+Architecture diagram of the Control-M/Agent clusters on IKP
+Explanation of key components and their functions within the architecture
+Relationship between Control-M/Agent clusters and other company applications
+2.Deployment Process
 
-## Introduction
+Step-by-step guide on how to deploy Control-M/Agent clusters on IKP
+Configuration details and best practices
+3.Monitoring and Logging
 
-Our Shared Agent Cluster IT Service is an innovative solution that allows application teams to schedule serverless jobs without the need to install and manage agent software on their servers. This service aims to improve efficiency and reduce the workload for application teams. With a centralized, shared agent cluster, you can streamline the execution of serverless tasks and focus on developing high-quality applications.
+Overview of monitoring and logging tools used with Control-M/Agent clusters and IKP
+Instructions on how to access logs and performance metrics
+Common log entries and what they indicate
+4.Troubleshooting and Issue Resolution
 
-## Target Use Cases
+List of common issues and their solutions for L1 support
+Escalation process and guidelines for L2 support
+Steps for diagnosing and resolving issues related to Control-M/Agent clusters and IKP
+5.Incident Management
 
-The Shared Agent Cluster IT Service is ideal for the following use cases:
+Incident management process and guidelines
+Roles and responsibilities of L1 and L2 support teams in incident management
+Communication channels and tools used during incident management
+6.Maintenance and Upgrades
 
-1. **Batch Executable By API**: Schedule and manage batch jobs efficiently through API calls, reducing manual intervention and improving overall application performance.
-2. **Semaphores Fetch by API**: Seamlessly fetch semaphores using API calls, simplifying the coordination of parallel tasks and ensuring smooth task execution.
-3. **Control-M Plugins with Centralized Connection Profiles**: Leverage Control-M plugins supported by centralized connection profiles, improving the management of connections for various applications and services.
+Scheduled maintenance windows and procedures
+Process for upgrading Control-M/Agent clusters and IKP
+Guidelines for notifying users about maintenance and upgrades
+7.Resources and Contact Information
 
-## Service Limitations
-
-Please be aware of the following limitations of the Shared Agent Cluster IT Service:
-
-1. **Job Output Retention**: As the service runs on the Internal Kubernetes (k8s) platform, we cannot guarantee that user job output will always be retained for a specific number of days. Users are encouraged to ensure they have appropriate backup and retention policies in place for their job outputs.
-
-## Onboarding Procedure
-
-To get started with the Shared Agent Cluster IT Service, follow the onboarding procedure outlined below:
-
-1. **Jira Service Desk**: Access our Jira Service Desk and create a new request to onboard the Shared Agent Cluster IT Service.
-2. **Application Details**: Provide the required information about your application, including the supported application kind, agent version, and job types.
-3. **Approval**: Our team will review your request and approve it if it meets the criteria for the Shared Agent Cluster IT Service.
-4. **Access**: Once approved, you will be granted access to the Shared Agent Cluster IT Service, allowing you to schedule and manage serverless jobs for your application.
-
-To begin the onboarding process, please visit our [Jira Service Desk](https://yourcompany.atlassian.net/servicedesk/).
+List of relevant documentation, websites, and forums for additional information
+Contact information for key personnel and teams involved in supporting Control-M/Agent clusters and IKP

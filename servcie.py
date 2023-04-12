7cef7db076f9fbe37c340ad0a2eac440fe387371
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-12 19:07:24 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -3,9 +3,6 @@ import shutil
 import bz2
 from datetime import datetime, timedelta
 
-import bz2
-from collections import defaultdict
-
 
 # Define the source file paths
 source_files = {'hk': '\\\\server\\pathhk', 'uk': '\\\\server2\\pathuk', 'us': '\\\\server3\\pathus'}
@@ -39,26 +36,51 @@ for region, path in source_files.items():
 # Create the sub-folder if it doesn't exist
 if not os.path.exists('./source'):
     os.makedirs('./source')
+    
+skip_existing_files = True
 
 # Loop through each file and download it to the local disk
 failed_files = []
-downloaded_files = []
+downloaded_files = []  # Added a list to store downloaded file information
 for file_info in file_list:
     try:
         # Copy the file to the local disk with the region prefix
         dest_filename = f"{file_info['region']}_{os.path.basename(file_info['file'])}"
         dest_path = os.path.join('./source', dest_filename)
-        shutil.copy(file_info['file'], dest_path)
-        
-        # Append the downloaded file information to the downloaded_files list
+
+        # Check if the file already exists in the './source' directory and skip the download if it does
+        if os.path.isfile(dest_path) and skip_existing_files:
+            print(f"Skipped file {file_info['file']} because it already exists in {dest_path}")
+        else:
+            if not os.path.isfile(dest_path):
+                shutil.copy(file_info['file'], dest_path)
+                print(f"Downloaded file {file_info['file']} to {dest_path}")
+            else:
+                print(f"File {file_info['file']} already exists in {dest_path}")
+            
+        # Append the file information to the downloaded_files list, whether it was downloaded or already existed
         downloaded_files.append({'region': file_info['region'], 'date': file_info['date'], 'file': dest_path})
         
-        # Print a success message
-        print(f"Downloaded file {file_info['file']} to {dest_path}")
     except Exception as e:
         # Add the failed file to the list and print a warning message
         failed_files.append(file_info)
         print(f"WARNING: Failed to download file {file_info['file']}: {e}")
+
+
+# Write the list of failed files to a log file
+if failed_files:
+    log_filename = f"./logs/history_{datetime.now().strftime('%Y%m%d')}.txt"
+    with open(log_filename, 'w') as log_file:
+        for file_info in failed_files:
+            log_file.write(f"Failed to download file {file_info['file']} for region {file_info['region']} on {file_info['date']}\n")
+    print(f"WARNING: {len(failed_files)} files failed to download. See log file {log_filename} for details.")
+else:
+    print("All files downloaded successfully.")
+
+# Output the list of downloaded file information
+print("Downloaded files information:")
+for info in downloaded_files:
+    print(info)
 
 audit_roles = ['role1', 'role2', 'role3']
 audit_accounts = []

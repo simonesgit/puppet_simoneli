de5a9df2c849f20f6e5f53568df2d9860a552d92
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-13 18:16:20 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -1,20 +1,12 @@
-# Rename the columns to avoid any conflicts with SQL keywords
-df_em = df_em.rename(columns={'USER_NOTE': 'user_note'})
-df_tpam = df_tpam.rename(columns={'TicketNbr': 'ticket_nbr'})
+# Define a custom function to find the corresponding row in df_tpam based on TicketNbr
+def find_tpamlogs_row(user_note, tpamlogs):
+    for idx, row in tpamlogs.iterrows():
+        if row['TicketNbr'] in user_note:
+            return row
+    return pd.Series(index=tpamlogs.columns)
 
-# Define a custom function to check if the ticket_nbr is a substring of the user_note
-def contains_ticket_nbr(user_note, ticket_nbr):
-    return ticket_nbr in user_note if user_note and ticket_nbr else False
+# Apply the function to the USER_NOTE column to create a new DataFrame with matched rows from df_tpam
+matched_tpam = df_em['USER_NOTE'].apply(find_tpamlogs_row, args=(df_tpam,))
 
-# Register the custom function with pandasql
-psql.register_function(contains_ticket_nbr, 'contains_ticket_nbr')
-
-# SQL query to merge the DataFrames using the custom function
-query = """
-SELECT a.*, b.*
-FROM df_em a
-LEFT JOIN df_tpam b
-ON contains_ticket_nbr(a.user_note, b.ticket_nbr)
-"""
-
-result_df = psql.sqldf(query, locals())
+# Now concatenate df_em and matched_tpam
+result_df = pd.concat([df_em, matched_tpam.reset_index(drop=True)], axis=1)

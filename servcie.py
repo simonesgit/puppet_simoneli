46f5a8c66dcdef912e6291d76cbd3d33510d6ee6
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-13 18:57:32 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -1,12 +1,28 @@
-# Define a custom function to find the corresponding row in df_tpam based on TicketNbr
-def find_tpamlogs_row(user_note, tpamlogs):
-    for idx, row in tpamlogs.iterrows():
-        if row['TicketNbr'] in user_note:
-            return row
-    return pd.Series(index=tpamlogs.columns)
+def save_dataframe_as_excel_xlsxwriter(df, filename, sheet_name='Sheet1', max_width_columns=None):
+    # Create a Pandas Excel writer using XlsxWriter as the engine
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
 
-# Apply the function to the USER_NOTE column to create a new DataFrame with matched rows from df_tpam
-matched_tpam = df_em['USER_NOTE'].apply(find_tpamlogs_row, args=(df_tpam,))
+    # Convert the DataFrame to an XlsxWriter Excel object
+    df.to_excel(writer, sheet_name=sheet_name, index=False)
 
-# Now concatenate df_em and matched_tpam
-result_df = pd.concat([df_em, matched_tpam.reset_index(drop=True)], axis=1)
+    # Get the xlsxwriter workbook and worksheet objects
+    workbook = writer.book
+    worksheet = writer.sheets[sheet_name]
+
+    # Adjust column widths
+    for col_idx, col in enumerate(df.columns):
+        if max_width_columns is None or col not in max_width_columns:
+            max_length = df[col].astype(str).str.len().max()
+            worksheet.set_column(col_idx, col_idx, max_length + 2)
+
+    # Close the Pandas Excel writer and output the Excel file using the save method of the workbook object
+    workbook.close()
+    
+    
+    
+    # Assuming you have a DataFrame called result_df
+filename = 'result_df.xlsx'
+sheet_name = 'Results'
+max_width_columns = ['Column1', 'Column2']  # Replace with the actual column names you want to skip
+
+save_dataframe_as_excel_xlsxwriter(result_df, filename, sheet_name, max_width_columns)

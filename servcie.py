ddd994a43a4b176d6cc5954633e6115d7ca8ee71
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-13 19:10:15 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -12,17 +12,10 @@ def save_dataframe_as_excel_xlsxwriter(df, filename, sheet_name='Sheet1', max_wi
     # Adjust column widths
     for col_idx, col in enumerate(df.columns):
         if max_width_columns is None or col not in max_width_columns:
-            max_length = df[col].astype(str).str.len().max()
+            max_data_length = df[col].astype(str).str.len().max()
+            max_col_name_length = len(col)
+            max_length = max(max_data_length, max_col_name_length)
             worksheet.set_column(col_idx, col_idx, max_length + 2)
 
     # Close the Pandas Excel writer and output the Excel file using the save method of the workbook object
     workbook.close()
-    
-    
-    
-    # Assuming you have a DataFrame called result_df
-filename = 'result_df.xlsx'
-sheet_name = 'Results'
-max_width_columns = ['Column1', 'Column2']  # Replace with the actual column names you want to skip
-
-save_dataframe_as_excel_xlsxwriter(result_df, filename, sheet_name, max_width_columns)

3c8098fe67232822851b2be2d2aff450d2a8eb05
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-13 18:10:50 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -1,19 +1,20 @@
-import pandasql as psql
-
-# Assuming you have two DataFrames: df_em and df_tpam
-
 # Rename the columns to avoid any conflicts with SQL keywords
 df_em = df_em.rename(columns={'USER_NOTE': 'user_note'})
 df_tpam = df_tpam.rename(columns={'TicketNbr': 'ticket_nbr'})
 
-# SQL query to merge the DataFrames
+# Define a custom function to check if the ticket_nbr is a substring of the user_note
+def contains_ticket_nbr(user_note, ticket_nbr):
+    return ticket_nbr in user_note if user_note and ticket_nbr else False
+
+# Register the custom function with pandasql
+psql.register_function(contains_ticket_nbr, 'contains_ticket_nbr')
+
+# SQL query to merge the DataFrames using the custom function
 query = """
 SELECT a.*, b.*
 FROM df_em a
 LEFT JOIN df_tpam b
-ON a.user_note LIKE '%' || b.ticket_nbr || '%'
+ON contains_ticket_nbr(a.user_note, b.ticket_nbr)
 """
 
 result_df = psql.sqldf(query, locals())
-
-print(result_df)

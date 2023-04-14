97c1b984bf4e2b513be5f3fb4858876cb8cf1886
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-14 11:23:46 +0800

    Update Nexus_test.sh

--- Nexus_test.sh
+++ Nexus_test.sh
@@ -1,57 +1,39 @@
-#!/bin/bash
+import pandas as pd
 
-# Set the Nexus Repository Manager base URL
-NEXUS_BASE_URL="http://nexus.example.com"
+def special_processing(df):
+    tmp_su, tmp_un, tmp_hn = None, None, None
 
-# Set the name of your hosted raw repository
-REPO_NAME="app-repo"
+    for index, row in df.iterrows():
+        if row['Operation'] == 'LOGIN USER':
+            tmp_su = row['System_Username']
+            tmp_un = row['Username']
+            tmp_hn = row['HOSTNAME']
 
-# Set the prefix of your software filename
-SOFTWARE_PREFIX="software"
+            # Query rows after the current row
+            for inner_index, inner_row in df.iloc[index+1:].iterrows():
+                if (inner_row['Username'] == tmp_un and
+                    inner_row['HOSTNAME'] == tmp_hn and
+                    (inner_row['System_Username'] == '' or pd.isna(inner_row['System_Username']))):
+                    # Set System_Username to the temporary stored value
+                    df.at[inner_index, 'System_Username'] = tmp_su
 
-# Set the directory where you want to download the latest software
-DOWNLOAD_DIR="/path/to/download/dir"
+# Sample data for testing
+data = {
+    'Operation': ['LOGIN USER', 'QUERY', 'LOGIN USER', 'QUERY'],
+    'System_Username': ['sys_user1', None, 'sys_user2', None],
+    'Username': ['user1', 'user1', 'user2', 'user2'],
+    'HOSTNAME': ['host1', 'host1', 'host2', 'host2']
+}
 
-# Get the list of available versions from Nexus Repository Manager
-VERSIONS=$(curl -s "${NEXUS_BASE_URL}/service/rest/v1/search/assets/download?repository=${REPO_NAME}&sort=version&direction=desc" | jq '.items[].version' | sort -r)
+df = pd.DataFrame(data)
 
-# Find the latest version
-for VERSION in $VERSIONS; do
-  SOFTWARE_URL="${NEXUS_BASE_URL}/repository/${REPO_NAME}/${VERSION}/${SOFTWARE_PREFIX}.zip"
-  if curl --head --silent --fail "$SOFTWARE_URL" > /dev/null; then
-    echo "Latest version is ${VERSION}"
-    echo "Downloading software from ${SOFTWARE_URL}..."
-    curl --fail --progress-bar --output "${DOWNLOAD_DIR}/${SOFTWARE_PREFIX}-${VERSION}.zip" "${SOFTWARE_URL}"
-    echo "Download complete."
-    exit 0
-  fi
-done
+# Fill missing values with empty strings
+df.fillna('', inplace=True)
 
-# If no valid version was found, print an error message
-echo "No valid version found."
-exit 1
+print("Before processing:")
+print(df)
 
+special_processing(df)
 
-
-#!/bin/bash
-
-# Prompt the user for their Nexus Repository Manager credentials
-read -p "Username: " NEXUS_USERNAME
-read -sp "Password: " NEXUS_PASSWORD
-echo
-
-# Set the Nexus Repository Manager base URL and repository name
-NEXUS_BASE_URL="http://nexus.example.com"
-REPO_NAME="app-repo"
-
-# Set the software filename prefix
-SOFTWARE_PREFIX="software"
-
-# Get the list of available versions of the software from Nexus Repository Manager
-VERSIONS=$(curl -sS -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" "${NEXUS_BASE_URL}/service/rest/v1/search/assets/download?repository=${REPO_NAME}&group=/${SOFTWARE_PREFIX}&sort=version&direction=desc" | jq -r '.items[].version' | sort -r)
-
-# Get the latest version of the software
-VERSION=$(echo "${VERSIONS}" | head -n 1)
-
-# Download the software file
-curl -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" -O "${NEXUS_BASE_URL}/repository/${REPO_NAME}/${VERSION}/${SOFTWARE_PREFIX}.zip"
+print("\nAfter processing:")
+print(df)

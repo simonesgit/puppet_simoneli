85e62210bbc945eed90acdddeff109b9fc00139e
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-17 17:22:40 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -1,10 +1,17 @@
-def find_tpam_row(user_note, username, df_tpam):
-    ticket_nbr = extract_ticket_nbr(user_note)
-    
-    if ticket_nbr:
-        matched_rows = df_tpam[(df_tpam["TicketNbr"] == ticket_nbr) & (df_tpam["AccountName"] == username)]
-        
-        if not matched_rows.empty:
-            return matched_rows.iloc[0]
+def special_processing(df):
+    user_mapping = {}
 
-    return pd.Series(dtype="object")
+    for index, row in df.iterrows():
+        if row['Operation'] == 'LOGIN USER':
+            key = (row['Username'], row['HOSTNAME'])
+            user_mapping[key] = row['System_Username']
+
+        elif row['Operation'] == 'LOGOUT USER':
+            key = (row['Username'], row['HOSTNAME'])
+            if key in user_mapping:
+                del user_mapping[key]
+
+        else:
+            key = (row['Username'], row['HOSTNAME'])
+            if key in user_mapping and (row['System_Username'] == '' or pd.isna(row['System_Username'])):
+                df.at[index, 'System_Username'] = user_mapping[key]

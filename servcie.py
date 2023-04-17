7480e044c5c92dcb3d1339b34300e54b2dea7982
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-17 15:52:17 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -1,9 +1,23 @@
-# Assuming you have a DataFrame called df
-columns_to_rename = ['old_column_name1', 'old_column_name2']  # Replace with the column names you want to rename
-new_column_names = ['new_column_name1', 'new_column_name2']  # Replace with the desired new column names
+import pandas as pd
 
-# Create a dictionary to map old column names to new column names only for the columns in columns_to_rename
-column_mapping = {old: new for old, new in zip(columns_to_rename, new_column_names)}
+# Create a sample dataframe
+data = {'TP_Reason': ["**PARDS**-Category: . Activity: Investigation. Detail: PA Task No:PAM456789, Child Task No:, Access Type:Read-Write, Summary for Request: To Access CCM."]}
+df = pd.DataFrame(data)
 
-# Rename columns using the column_mapping dictionary
-df = df.rename(columns=column_mapping)
+# Define the regular expression patterns for each new column
+category_pattern = r"Category:\s*(.*?)[. ]"
+activity_pattern = r"Activity:\s*(.*?)[. ]"
+pa_task_no_pattern = r"PA Task No:\s*(.*?)[, ]"
+child_task_no_pattern = r"Child Task No:\s*(.*?)[, ]"
+access_type_pattern = r"Access Type:\s*(.*?)[, ]"
+summary_pattern = r"Summary for Request:\s*(.*)"
+
+# Use str.extract to create new columns from the TP_Reason column
+df['TP_Category'] = df['TP_Reason'].str.extract(category_pattern)
+df['TP_Activity'] = df['TP_Reason'].str.extract(activity_pattern)
+df['TP_PATaskNo'] = df['TP_Reason'].str.extract(pa_task_no_pattern)
+df['TP_ChildTaskNo'] = df['TP_Reason'].str.extract(child_task_no_pattern)
+df['TP_AccessType'] = df['TP_Reason'].str.extract(access_type_pattern)
+df['TP_Summary'] = df['TP_Reason'].str.extract(summary_pattern)
+
+print(df)

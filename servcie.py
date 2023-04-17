f8a745ed8498fabdf24a67113f938c83b2cf0445
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-17 16:06:27 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -1,25 +1,10 @@
-import pandas as pd
+def find_tpam_row(user_note, username, df_tpam):
+    ticket_nbr = extract_ticket_nbr(user_note)
+    
+    if ticket_nbr:
+        matched_rows = df_tpam[(df_tpam["TicketNbr"] == ticket_nbr) & (df_tpam["AccountName"] == username)]
+        
+        if not matched_rows.empty:
+            return matched_rows.iloc[0]
 
-# Create a sample dataframe
-data = {'TP_Reason': ["**PARDS**-Category: . Activity: Investigation. Detail: PA Task No:PAM456789, Child Task No:, Access Type:Read-Write, Summary for Request: To Access CCM."]}
-df = pd.DataFrame(data)
-
-# Define the regular expression patterns for each new column
-category_pattern = r"Category:\s*(.*?)[. ]"
-activity_pattern = r"Activity:\s*(.*?)[. ]"
-pa_task_no_pattern = r"PA Task No:\s*(.*?)[, ]"
-child_task_no_pattern = r"Child Task No:\s*(.*?)[, ]"
-access_type_pattern = r"Access Type:\s*(.*?)[, ]"
-summary_pattern = r"Summary for Request:\s*(.*)"
-
-# Use str.extract to create new columns from the TP_Reason column
-df['TP_Category'] = df['TP_Reason'].str.extract(category_pattern)
-df['TP_Activity'] = df['TP_Reason'].str.extract(activity_pattern)
-df['TP_PATaskNo'] = df['TP_Reason'].str.extract(pa_task_no_pattern)
-df['TP_ChildTaskNo'] = df['TP_Reason'].str.extract(child_task_no_pattern)
-df['TP_AccessType'] = df['TP_Reason'].str.extract(access_type_pattern)
-df['TP_Summary'] = df['TP_Reason'].str.extract(summary_pattern)
-
-print(df)
-
-
+    return pd.Series(dtype="object")

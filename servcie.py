357501abd1416443341b59ccf7c7075a06727a20
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-18 10:24:33 +0800

    Update servcie.py

--- servcie.py
+++ servcie.py
@@ -1,8 +1,9 @@
-# Create an empty DataFrame with the same columns as df_em and the same number of rows as unmatched_tpam
-empty_df_em = pd.DataFrame(np.nan, index=range(len(unmatched_tpam)), columns=df_em.columns)
+OpenID Connect (OIDC) is an authentication protocol built on top of the OAuth 2.0 authorization framework. It allows clients to verify the identity of users and obtain basic profile information from identity providers (IdPs) in a standardized manner. OIDC is commonly used in Kubernetes (k8s) to provide secure authentication for users and services interacting with the cluster.
 
-# Concatenate empty_df_em with unmatched_tpam
-unmatched_combined = pd.concat([empty_df_em.reset_index(drop=True), unmatched_tpam.reset_index(drop=True)], axis=1)
+Here's a brief introduction to OIDC in Kubernetes that you can share with your colleagues:
 
-# Concatenate df_result with unmatched_combined
-df_result = pd.concat([df_result, unmatched_combined], ignore_index=True)
+What is OIDC?
+OIDC stands for OpenID Connect, a simple and secure authentication protocol built on top of the OAuth 2.0 framework. It enables applications to authenticate users and obtain their basic profile information from identity providers (IdPs) in a standardized way.
+
+How does OIDC work in Kubernetes?
+Kubernetes supports OIDC for authentication purposes, allowing you to manage user access to your cluster securely. When you enable OIDC in Kubernetes, the API server communicates with an external IdP to authenticate users. This way, Kubernetes can verify user identities and manage access control based on their assigned roles and permissions.

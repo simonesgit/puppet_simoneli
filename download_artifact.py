272e7f6ee38a5acf6b7448e0b63f76d8b5496444
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-04-21 13:50:33 +0800

    Update download_artifact.py

--- download_artifact.py
+++ download_artifact.py
@@ -2,25 +2,29 @@ import os
 import requests
 from urllib.parse import urljoin
 
-# Replace with your Nexus URL, credentials, repository name, and local storage path
+# Replace with your Nexus URL, credentials, repository name, and group filter
 NEXUS_URL = "https://nexus.example.com"
 USERNAME = "your_username"
 PASSWORD = "your_password"
 REPOSITORY = "your_repository"
-LOCAL_STORAGE_PATH = "/path/to/local/storage"
+GROUP_FILTER = "your_group"
 
 # Nexus REST API endpoint
 SEARCH_API = "/service/rest/v1/search"
 
-def get_all_artifacts(repository):
+# Local storage path
+LOCAL_STORAGE_PATH = "./download"
+
+def get_all_artifacts(repository, group_filter):
     artifacts = []
     url = urljoin(NEXUS_URL, SEARCH_API)
     params = {
         "repository": repository,
+        "group": f"{group_filter}*",
     }
 
     while url:
-        response = requests.get(url, params=params, auth=(USERNAME, PASSWORD))
+        response = requests.get(url, params=params, auth=(USERNAME, PASSWORD), verify=False)
         response.raise_for_status()
         data = response.json()
 
@@ -37,7 +41,7 @@ def download_artifact(artifact, local_storage_path):
 
     os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
 
-    response = requests.get(download_url, auth=(USERNAME, PASSWORD), stream=True)
+    response = requests.get(download_url, auth=(USERNAME, PASSWORD), verify=False, stream=True)
     response.raise_for_status()
 
     with open(local_file_path, "wb") as f:
@@ -47,7 +51,7 @@ def download_artifact(artifact, local_storage_path):
     print(f"Downloaded {download_url} to {local_file_path}")
 
 def main():
-    artifacts = get_all_artifacts(REPOSITORY)
+    artifacts = get_all_artifacts(REPOSITORY, GROUP_FILTER)
 
     for artifact in artifacts:
         download_artifact(artifact, LOCAL_STORAGE_PATH)

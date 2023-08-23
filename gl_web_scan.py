c382daed4180dc03f798bc3e08825fb99a82fc16
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-08-23 14:24:58 +0800

    Update gl_web_scan.py

--- gl_web_scan.py
+++ gl_web_scan.py
@@ -1,33 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 # Replace <repository_url> with the actual URL of your repository
-repository_url = "http://<repository_url>/repos/rhel/8/x86_64/"
+repository_url = "https://<repository_url>/repos/rhel/8/x86_64/"
 
-response = requests.get(repository_url)
-if response.status_code == 200:
-    content = response.text
+def get_folder_contents(url):
+    response = requests.get(url, verify=True)
+    if response.status_code == 200:
+        content = response.text
+        soup = BeautifulSoup(content, "html.parser")
+        links = soup.find_all("a")
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(content, "html.parser")
+        folder_and_file_names = []
+        for link in links:
+            name = link.get("href")
+            if name.endswith("/"):  # If the name ends with a forward slash, it represents a folder
+                folder_url = urljoin(url, name)
+                subfolder_contents = get_folder_contents(folder_url)
+                folder_and_file_names.extend(subfolder_contents)
+            else:
+                folder_and_file_names.append(f"[File] {name}")
 
-    # Find all <a> tags which represent links
-    links = soup.find_all("a")
+        return folder_and_file_names
+    else:
+        print(f"Error: Failed to retrieve the content of {url}. Status code: {response.status_code}")
+        return []
 
-    # Extract folder and file names from the links
-    folder_and_file_names = []
-    for link in links:
-        name = link.get("href")
-        if name.endswith("/"):  # If the name ends with a forward slash, it represents a folder
-            folder_and_file_names.append(f"[Folder] {name}")
-        else:
-            folder_and_file_names.append(f"[File] {name}")
+# Starting point: list the contents of the root repository URL
+folder_and_file_names = get_folder_contents(repository_url)
 
-    # Store the list of folders and files to a local file
-    output_file = "folder_and_file_list.txt"
-    with open(output_file, "w") as file:
-        file.write("\n".join(folder_and_file_names))
+# Store the list of folders and files to a local file
+output_file = "folder_and_file_list.txt"
+with open(output_file, "w") as file:
+    file.write("\n".join(folder_and_file_names))
 
-    print(f"List of folders and files under {repository_url} has been saved to {output_file}.")
-else:
-    print(f"Error: Failed to retrieve the repository content. Status code: {response.status_code}")
+print(f"List of folders and files under {repository_url} and its subfolders has been saved to {output_file}.")

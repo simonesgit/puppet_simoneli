ac6e978500ac268bc031c01090be3594f93d0187
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-09-01 17:23:46 +0800

    Update vuln_remove.sh

--- vuln_remove.sh
+++ vuln_remove.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-FILES="/path/file1" 
-FILES+="/path/file2 "
+FILES="/path/file1"
+FILES+="/path/file2"  
 FILES+="/path/file3"
 
 for f in $FILES; do
   if [ -f "$f" ]; then
     echo "Removing $f"
-    rm -f "$f" 
+    rm -f "$f"
   fi
 done
 
+remaining=()
 for f in $FILES; do
   if [ -f "$f" ]; then
-    echo "WARNING: Unable to clean up vulnerable file $f. Please remove manually."
+    remaining+=("$f")
   fi
 done
+
+if [ ${#remaining[@]} -gt 0 ]; then
+  echo "WARNING: Unable to clean up vulnerable files: ${remaining[@]}. Please remove manually."
+fi
 
 echo "Verification complete."

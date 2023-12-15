dc29670178a77d30e3f4575dcf915a134fcb5a70
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-12-15 11:49:36 +0800

    Update confluence_import.py

--- confluence_import.py
+++ confluence_import.py
@@ -6,32 +6,47 @@ api_url = 'https://your-confluence-site/wiki/rest/api/content/{page_id}'
 # Page ID of the page you want to update
 page_id = '12345678'
 
-# Retrieve the existing page version
-response = requests.get(api_url.format(page_id=page_id))
-if response.status_code == 200:
-    existing_version = response.json().get('version', {}).get('number', 0)
-else:
-    print(f'Failed to retrieve existing page version. Status code: {response.status_code}. Error message: {response.text}')
-    existing_version = 0
+# Credentials for authentication
+username = 'your-username'
+password = 'your-password'
 
-# Prepare the API request payload
-payload = {
-    'version': {
-        'number': existing_version + 1  # Increase the existing version number to update the content
-    },
-    'body': {
-        'storage': {
-            'value': html_content,
-            'representation': 'storage'
+# Path to the CA certificate file for SSL verification
+ca_cert_path = '/path/to/ca.pem'
+
+# Function to retrieve the current version of the page
+def get_page_version():
+    response = requests.get(api_url.format(page_id=page_id), auth=(username, password), verify=ca_cert_path)
+    if response.status_code == 200:
+        return response.json().get('version', {}).get('number', 0)
+    else:
+        print(f'Failed to retrieve existing page version. Status code: {response.status_code}. Error message: {response.text}')
+        return 0
+
+# Function to update the page content
+def update_page_content(version, content):
+    payload = {
+        'version': {
+            'number': version + 1  # Increase the existing version number to update the content
+        },
+        'body': {
+            'storage': {
+                'value': content,
+                'representation': 'storage'
+            }
         }
     }
-}
+    response = requests.put(api_url.format(page_id=page_id), json=payload, auth=(username, password), verify=ca_cert_path, headers={'Content-Type': 'application/json'})
+    if response.status_code == 200:
+        print('Page content updated successfully.')
+    else:
+        print(f'Failed to update page content. Status code: {response.status_code}. Error message: {response.text}')
 
-# Make the API request
-response = requests.put(api_url.format(page_id=page_id), json=payload, headers={'Content-Type': 'application/json'})
+# Read the HTML content from file
+with open('project.html', 'r') as file:
+    html_content = file.read()
 
-# Check the response status
-if response.status_code == 200:
-    print('Page content updated successfully.')
-else:
-    print(f'Failed to update page content. Status code: {response.status_code}. Error message: {response.text}')
+# Get the current version of the page
+current_version = get_page_version()
+
+# Update the page content
+update_page_content(current_version, html_content)

74e535fa17fec42ab23c16225a1d07259e6f4f83
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-12-15 11:41:25 +0800

    Update confluence_import.py

--- confluence_import.py
+++ confluence_import.py
@@ -1,41 +1,37 @@
 import requests
 
-def update_confluence_page_content(space_key, page_id=None, page_title=None, username=None, password=None):
-    # Set the Confluence API endpoint
-    api_url = f"https://your-confluence-url/rest/api/content/{page_id or page_title}"
+# Confluence API endpoint for retrieving and updating page content
+api_url = 'https://your-confluence-site/wiki/rest/api/content/{page_id}'
 
-    # Set the request headers
-    headers = {
-        "Content-Type": "application/json"
-    }
+# Page ID of the page you want to update
+page_id = '12345678'
 
-    # Set the request parameters
-    params = {
-        "spaceKey": space_key
-    }
+# Retrieve the existing page version
+response = requests.get(api_url.format(page_id=page_id))
+if response.status_code == 200:
+    existing_version = response.json().get('version', {}).get('number', 0)
+else:
+    print(f'Failed to retrieve existing page version. Status code: {response.status_code}. Error message: {response.text}')
+    existing_version = 0
 
-    # Read the new page content from the file
-    with open("confluence_page_content.txt", "r", encoding="utf-8") as file:
-        new_content = file.read()
-
-    # Create the request payload
-    payload = {
-        "version": {
-            "number": 1  # Increment the version number to update the page
-        },
-        "title": page_title,  # Only needed if updating by title
-        "type": "page",
-        "body": {
-            "storage": {
-                "value": new_content,
-                "representation": "storage"
-            }
+# Prepare the API request payload
+payload = {
+    'version': {
+        'number': existing_version + 1  # Increase the existing version number to update the content
+    },
+    'body': {
+        'storage': {
+            'value': html_content,
+            'representation': 'storage'
         }
     }
+}
 
-    # Send the request to update page content
-    response = requests.put(api_url, headers=headers, params=params, auth=(username, password), json=payload, verify="ca.pem")
-    response.raise_for_status()
+# Make the API request
+response = requests.put(api_url.format(page_id=page_id), json=payload, headers={'Content-Type': 'application/json'})
 
-# Usage example
-update_confluence_page_content(space_key="your-space-key", page_id="12345", username="your-username", password="your-password")
+# Check the response status
+if response.status_code == 200:
+    print('Page content updated successfully.')
+else:
+    print(f'Failed to update page content. Status code: {response.status_code}. Error message: {response.text}')

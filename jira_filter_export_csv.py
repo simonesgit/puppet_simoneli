24f04509277de9782f63eeb514607b6d36003a17
simonesgit <56809829+simonesgit@users.noreply.github.com>
2023-12-28 23:47:37 +0800

    Update jira_filter_export_csv.py

--- jira_filter_export_csv.py
+++ jira_filter_export_csv.py
@@ -5,9 +5,6 @@ import pandas as pd
 filter_url = "https://your-jira-instance/rest/api/2/search?jql=filter={}"
 filter_id = '234567'
 
-# Fields to filter
-filter_fields = []  # Add the desired field names to this list
-
 # Jira credentials
 username = "your_username"
 password = "your_password"
@@ -17,6 +14,25 @@ ca_file_path = "/path/to/your/ca_file.pem"
 
 # Set the maximum number of results per page
 max_results = 200
+
+# Customized field ID to name mapping
+field_id_to_name = {
+    "customfield_1": "Custom Field 1",
+    "customfield_2": "Custom Field 2",
+    # Add more field ID to name mappings as needed
+}
+
+def convert_field_names(fields):
+    """
+    Converts the names of customized fields in the given fields dictionary.
+    """
+    converted_fields = {}
+    for field, value in fields.items():
+        if field in field_id_to_name:
+            converted_fields[field_id_to_name[field]] = value
+        else:
+            converted_fields[field] = value
+    return converted_fields
 
 # Send HTTP GET request to retrieve Jira filter results
 response = requests.get(filter_url.format(filter_id), auth=(username, password), verify=ca_file_path, params={'maxResults': max_results})
@@ -41,18 +57,21 @@ if response.status_code == 200:
         key = issue['key']
         fields = issue['fields']
 
+        # Convert the field names in the fields dictionary
+        converted_fields = convert_field_names(fields)
+
         # Filter fields based on filter_fields list
         if filter_fields:
-            fields = {field: value for field, value in fields.items() if field in filter_fields}
+            converted_fields = {field: value for field, value in converted_fields.items() if field in filter_fields}
         else:
             # Remove fields with no contents
-            fields = {field: value for field, value in fields.items() if value}
+            converted_fields = {field: value for field, value in converted_fields.items() if value}
 
         # Move 'key' field to the beginning of the dictionary
-        fields = {'key': key, **fields}
+        converted_fields = {'key': key, **converted_fields}
 
         # Append the fields dictionary to the issue_details list
-        issue_details.append(fields)
+        issue_details.append(converted_fields)
 
     # Create a pandas DataFrame from the issue_details list
     df = pd.DataFrame(issue_details)
